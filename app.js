d8a04cbc219780e7e7668381684866a7c50126c3
Theonrex <theonrex1@gmail.com>
2022-06-21 09:41:41 +0100

    changes

---
+++ app.js
@@ -0,0 +1,4 @@
+document.getElementById("name").innerHTML = "Olawande Armstrong";
+document.getElementById("height").innerHTML = " 178cm";
+document.getElementById("country").innerHTML = " Nigeria";
+
